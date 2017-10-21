daf0127b9c52fe52d17fc6bd17f80b47906a7789
天 <tiancheng@taotaosou.com>
2017-10-21 11:33:31 +0800

    save

--- src/store/index.js
+++ src/store/index.js
@@ -1,33 +1,34 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-Vue.use(Vuex); 
- 
-const store = new Vuex.Store({  
-  state: {  
-    count: 10,  
-    numb: 10086  
-  }, 
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    count: 10,
+    numb: 10086
+  },
     mutations: {
     increment: state => state.count++,
     decrement: state => state.count--
-  } 
-  // getters: {  
-  //   add: (state, getter) => {  
-  //     state.count = getter.add;  
-  //     return state.count;  
-  //   },  
-  // },  
-  // mutations: {  
-  //   increment(state,){  
-  //       state.count += 2;  
-  //   },  
-  // },  
-  // actions: {  
-  //   actionA({ dispatch, commit }) {  
-  //     return commit('add');  
-  //   },  
-  // }  
-});  
-  
-export default store; 
+  }
+   
+  // getters: {
+  //   add: (state, getter) => {
+  //     state.count = getter.add;
+  //     return state.count;
+  //   },
+  // },
+  // mutations: {
+  //   increment(state,){
+  //       state.count += 2;
+  //   },
+  // },
+  // actions: {
+  //   actionA({ dispatch, commit }) {
+  //     return commit('add');
+  //   },
+  // }
+});
+
+export default store;
